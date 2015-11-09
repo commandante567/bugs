ce90fa7a1e9581e78f71a48f8f8e531a754ee0ee
commandante567 <egor.semenof@yandex.ru>
2015-11-09 16:54:07 +0300

    Small fp fix

--- front-page.php
+++ front-page.php
@@ -54,7 +54,7 @@
 
             <div class="pluses clearfix">
 <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/24.png" alt=""/>
-            <span>Мы работаем ежедневно круглосуточно без обеда и праздников на защите своих клиентов в СПб и ЛО!</span>
+            <span>Мы работаем ежедневно круглосуточно без обеда и праздников на защите своих клиентов в в Ярославле и Ярославской области</span>
             </div>
 
             <div class="pluses clearfix"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/4des.png" alt=""/>
